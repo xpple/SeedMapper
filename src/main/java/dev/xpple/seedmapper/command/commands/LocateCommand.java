8f6934d2ab9e6579214ed6537544b82e24da267c
xpple <fred@xpple.dev>
2024-11-05 15:49:21 +0100

    Extract spiral loop to class + create tests

--- src/main/java/dev/xpple/seedmapper/command/commands/LocateCommand.java
+++ src/main/java/dev/xpple/seedmapper/command/commands/LocateCommand.java
@@ -9,6 +9,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.xpple.seedmapper.command.CommandExceptions;
 import dev.xpple.seedmapper.command.CustomClientCommandSource;
+import dev.xpple.seedmapper.util.SpiralLoop;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -19,7 +20,6 @@ import net.minecraft.world.level.levelgen.WorldgenRandom;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
-import java.util.function.Supplier;
 
 import static dev.xpple.seedmapper.command.arguments.BiomeArgument.*;
 import static dev.xpple.seedmapper.command.arguments.StructureArgument.*;
@@ -53,7 +53,7 @@ public class LocateCommand {
 
             BlockPos center = BlockPos.containing(source.getPosition());
 
-            spiral(center.getX(), center.getZ(), 6400, 32, (x, z) -> {
+            SpiralLoop.spiral(center.getX(), center.getZ(), 6400, 32, (x, z) -> {
                 if (CubiomesHeaders.getBiomeAt(generator, 1, x, 63, z) != biome) {
                     return false;
                 }
@@ -91,7 +91,7 @@ public class LocateCommand {
 
             BlockPos center = BlockPos.containing(source.getPosition());
             int regionSize = StructureConfig.regionSize(structureConfig) << 4;
-            spiral(center.getX() / regionSize, center.getZ() / regionSize, Level.MAX_LEVEL_SIZE / regionSize, (x, z) -> {
+            SpiralLoop.spiral(center.getX() / regionSize, center.getZ() / regionSize, Level.MAX_LEVEL_SIZE / regionSize, (x, z) -> {
                 MemorySegment structurePos = Pos.allocate(arena);
                 int valid = CubiomesHeaders.getStructurePos(structure, version, seed, x, z, structurePos);
                 if (valid == 0) {
@@ -124,7 +124,7 @@ public class LocateCommand {
         }
         long seed = source.getSeed().getSecond();
         ChunkPos center = new ChunkPos(BlockPos.containing(source.getPosition()));
-        spiral(center.x, center.z, 6400, (x, z) -> {
+        SpiralLoop.spiral(center.x, center.z, 6400, (x, z) -> {
             RandomSource random = WorldgenRandom.seedSlimeChunk(x, z, seed, 987234911L);
             if (random.nextInt(10) == 0) {
                 int blockPosX = (x << 4) + 9;
@@ -156,36 +156,5 @@ public class LocateCommand {
             return false;
         }, CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownCommand()::create);
         return Command.SINGLE_SUCCESS;
-    }
-
-    private static void spiral(final int centerX, final int centerZ, final int radius, CoordinateCallback callback, Supplier<? extends CommandSyntaxException> failure) throws CommandSyntaxException {
-        spiral(centerX, centerZ, radius, 1, callback, failure);
-    }
-
-    private static void spiral(final int centerX, final int centerZ, final int radius, final int step, CoordinateCallback callback, Supplier<? extends CommandSyntaxException> failure) throws CommandSyntaxException {
-        int x = centerX, dx = 0, z = centerZ, dz = -step;
-        final int leftBoundX = centerX - radius, rightBoundX = centerX + radius;
-        final int bottomBoundZ = centerZ - radius, topBoundZ = centerZ + radius;
-        final int max = (2 * radius) * (2 * radius);
-        for (int i = 0; i < max ; i++) {
-            if (leftBoundX <= x && x <= rightBoundX && bottomBoundZ <= z && z <= topBoundZ) {
-                if (callback.consume(x, z)) {
-                    return;
-                }
-            }
-            if (x - centerX == z - centerZ || (x < centerX && x - centerX == centerZ - z) || (x > centerX && x - centerX == centerZ + step - z)) {
-                final int temp = dx;
-                dx = -dz;
-                dz = temp;
-            }
-            x += dx;
-            z += dz;
-        }
-        throw failure.get();
-    }
-
-    @FunctionalInterface
-    private interface CoordinateCallback {
-        boolean consume(int x, int z);
     }
 }
---
+++ src/main/java/dev/xpple/seedmapper/util/SpiralLoop.java
@@ -0,0 +1,42 @@
+package dev.xpple.seedmapper.util;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import java.util.function.Supplier;
+
+public final class SpiralLoop {
+
+    private SpiralLoop() {
+    }
+
+    public static void spiral(final int centerX, final int centerZ, final int radius, CoordinateCallback callback, Supplier<? extends CommandSyntaxException> failure) throws CommandSyntaxException {
+        spiral(centerX, centerZ, radius, 1, callback, failure);
+    }
+
+    public static void spiral(final int centerX, final int centerZ, final int radius, final int step, CoordinateCallback callback, Supplier<? extends CommandSyntaxException> failure) throws CommandSyntaxException {
+        int x = centerX, dx = 0, z = centerZ, dz = -step;
+        final int leftBoundX = centerX - radius, rightBoundX = centerX + radius;
+        final int bottomBoundZ = centerZ - radius, topBoundZ = centerZ + radius;
+        final int max = (2 * radius + 1) * (2 * radius + 1);
+        for (int i = 0; i < max ; i++) {
+            if (leftBoundX <= x && x <= rightBoundX && bottomBoundZ <= z && z <= topBoundZ) {
+                if (callback.consume(x, z)) {
+                    return;
+                }
+            }
+            if (x - centerX == z - centerZ || (x < centerX && x - centerX == centerZ - z) || (x > centerX && x - centerX == centerZ + step - z)) {
+                final int temp = dx;
+                dx = -dz;
+                dz = temp;
+            }
+            x += dx;
+            z += dz;
+        }
+        throw failure.get();
+    }
+
+    @FunctionalInterface
+    public interface CoordinateCallback {
+        boolean consume(int x, int z);
+    }
+}
