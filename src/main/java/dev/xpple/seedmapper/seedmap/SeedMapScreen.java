e9990e63b829b181c052c4a92447679780549a60
Crosby <32882447+crosby-moe@users.noreply.github.com>
2025-12-01 16:20:10 -0500

    skip intersection checks by directly submitting to current layer (#104)
    * skip intersection checks by directly submitting to current layer
    
    * oops, all slime chunks!
    
    * Add comment
    
    ---------
    
    Co-authored-by: Frederik van der Els <fred@xpple.dev>

--- src/main/java/dev/xpple/seedmapper/seedmap/SeedMapScreen.java
+++ src/main/java/dev/xpple/seedmapper/seedmap/SeedMapScreen.java
@@ -17,6 +17,7 @@ import com.github.cubiomes.SurfaceNoise;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.textures.GpuTextureView;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.xpple.seedmapper.SeedMapper;
 import dev.xpple.seedmapper.command.arguments.CanyonCarverArgument;
@@ -47,6 +48,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.PlayerFaceRenderer;
+import net.minecraft.client.gui.render.TextureSetup;
+import net.minecraft.client.gui.render.state.BlitRenderState;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.client.input.MouseButtonEvent;
@@ -76,6 +79,7 @@ import net.minecraft.world.level.levelgen.PositionalRandomFactory;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
 import net.minecraft.world.level.levelgen.XoroshiroRandomSource;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix3x2f;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemoryLayout;
@@ -327,6 +331,8 @@ public class SeedMapScreen extends Screen {
             }
         }
 
+        guiGraphics.nextStratum();
+
         int horChunkRadius = Math.ceilDiv(this.seedMapWidth / 2, SCALED_CHUNK_SIZE * Configs.PixelsPerBiome);
         int verChunkRadius = Math.ceilDiv(this.seedMapHeight / 2, SCALED_CHUNK_SIZE * Configs.PixelsPerBiome);
 
@@ -363,6 +369,8 @@ public class SeedMapScreen extends Screen {
                     }
                 }
             });
+
+        guiGraphics.nextStratum();
 
         // draw strongholds
         if (this.toggleableFeatures.contains(MapFeature.STRONGHOLD) && Configs.ToggledFeatures.contains(MapFeature.STRONGHOLD)) {
@@ -1060,7 +1068,13 @@ public class SeedMapScreen extends Screen {
         static void drawFeatureIcon(GuiGraphics guiGraphics, MapFeature.Texture texture, int minX, int minY, int colour) {
             int iconWidth = texture.width();
             int iconHeight = texture.height();
-            guiGraphics.blit(RenderPipelines.GUI_TEXTURED, texture.resourceLocation(), minX, minY, 0, 0, iconWidth, iconHeight, iconWidth, iconHeight, colour);
+
+            // Skip intersection checks (GuiRenderState.hasIntersection) you would otherwise get when calling
+            // GuiGraphics.blit(RenderPipeline, ResourceLocation, int, int, float, float, int, int, int, int, int)
+            // as these checks incur a significant performance hit
+            GpuTextureView gpuTextureView = Minecraft.getInstance().getTextureManager().getTexture(texture.resourceLocation()).getTextureView();
+            BlitRenderState renderState = new BlitRenderState(RenderPipelines.GUI_TEXTURED, TextureSetup.singleTexture(gpuTextureView), new Matrix3x2f(guiGraphics.pose()), minX, minY, minX + iconWidth, minY + iconHeight, 0, 1, 0, 1, colour, guiGraphics.scissorStack.peek());
+            guiGraphics.guiRenderState.submitBlitToCurrentLayer(renderState);
         }
     }
 
--- src/main/resources/seedmapper.aw
+++ src/main/resources/seedmapper.aw
@@ -1,5 +1,4 @@
 accessWidener v2 named
-accessible class net/minecraft/client/renderer/RenderType$CompositeRenderType
 accessible class net/minecraft/client/renderer/RenderType$CompositeState
 
 accessible field net/minecraft/world/level/biome/BiomeManager biomeZoomSeed J
