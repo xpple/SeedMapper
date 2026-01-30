26fe84d9d7d24ffda0be47c4417f1c30487dd7eb
Frederik van der Els <fred@xpple.dev>
2026-01-30 19:51:26 +0100

    Add suspicious stew effect support

--- buildSrc/src/main/java/dev/xpple/seedmapper/buildscript/CreateJavaBindingsTask.java
+++ buildSrc/src/main/java/dev/xpple/seedmapper/buildscript/CreateJavaBindingsTask.java
@@ -13,6 +13,6 @@ public abstract class CreateJavaBindingsTask extends Exec {
 
         this.setWorkingDir(this.getProject().getRootDir());
         this.setStandardOutput(System.out);
-        this.commandLine("./jextract/build/jextract/bin/jextract" + EXTENSION, "--include-dir", "src/main/c/cubiomes", "--output", "src/main/java", "--use-system-load-library", "--target-package", "com.github.cubiomes", "--header-class-name", "Cubiomes", "@includes.txt", "biomenoise.h", "biomes.h", "finders.h", "generator.h", "layers.h", "biomenoise.h", "biomes.h", "noise.h", "terrainnoise.h", "rng.h", "util.h", "quadbase.h", "xrms.h", "loot/items.h", "loot/logging.h", "loot/loot_functions.h", "loot/loot_table_context.h", "loot/loot_table_parser.h", "loot/loot_tables.h", "loot/mc_loot.h");
+        this.commandLine("./jextract/build/jextract/bin/jextract" + EXTENSION, "--include-dir", "src/main/c/cubiomes", "--output", "src/main/java", "--use-system-load-library", "--target-package", "com.github.cubiomes", "--header-class-name", "Cubiomes", "@src/main/c/cubiomes/includes.txt", "biomenoise.h", "biomes.h", "finders.h", "generator.h", "layers.h", "biomenoise.h", "biomes.h", "noise.h", "terrainnoise.h", "rng.h", "util.h", "quadbase.h", "xrms.h", "loot/items.h", "loot/logging.h", "loot/loot_functions.h", "loot/loot_table_context.h", "loot/loot_table_parser.h", "loot/loot_tables.h", "loot/mc_loot.h");
     }
 }
