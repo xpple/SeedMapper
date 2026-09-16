4e10764429896efa66bb276c4bcbb67c0275f452
Frederik van der Els <fred@xpple.dev>
2026-09-16 13:46:00 +0200

    Fix rendering when improved transparency (OIT) is enabled

--- src/main/java/dev/xpple/seedmapper/render/NoDepthLayer.java
+++ src/main/java/dev/xpple/seedmapper/render/NoDepthLayer.java
@@ -6,6 +6,7 @@ import com.mojang.renderpearl.api.pipeline.DepthStencilState;
 import com.mojang.renderpearl.api.pipeline.RenderPipeline;
 import dev.xpple.seedmapper.SeedMapper;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.oit.OitPipelineSet;
 import net.minecraft.client.renderer.rendertype.LayeringTransform;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
@@ -23,8 +24,14 @@ public final class NoDepthLayer {
             .build()
     );
 
+    private static final OitPipelineSet LINES_NO_DEPTH_OIT_PIPELINE = OitPipelineSet.builder(
+        SeedMapper.MOD_ID + "_no_depth", RenderPipeline.builder(RenderPipelines.OIT_LINES_SNIPPET))
+            .withoutDepthTest()
+            .build();
+
     public static final RenderType LINES_NO_DEPTH_LAYER = RenderType.create(SeedMapper.MOD_ID + "_no_depth",
         RenderSetup.builder(LINES_NO_DEPTH_PIPELINE)
+            .setOitPipelines(LINES_NO_DEPTH_OIT_PIPELINE)
             .setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
             .createRenderSetup()
     );
